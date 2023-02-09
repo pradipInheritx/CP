141b70fdcfd37bd8ca5c2bba0783e34346f214e0
Mukut Prasad <mukutprasad1992@gmail.com>
2023-02-09 19:10:13 +0530

    Worked on API integration

--- functions/src/common/models/Coin.ts
+++ functions/src/common/models/Coin.ts
@@ -1,8 +1,8 @@
-//import { ICryptoSnapshotTickers } from "@polygon.io/client-js";
+// import { ICryptoSnapshotTickers } from "@polygon.io/client-js";
 import coinList from "../consts/coins.json";
-import { firestore } from "firebase-admin";
+import {firestore} from "firebase-admin";
 // import { rest } from "./Ajax";
-import { CPVIObj, cpviTaskCoin, cpviTaskPair } from "./CPVI";
+import {CPVIObj, cpviTaskCoin, cpviTaskPair} from "./CPVI";
 import moment from "moment";
 import axios from "axios";
 import {
@@ -51,21 +51,21 @@ export const filterCoins: (
   input: { [p: string]: Coin },
   allCoins: string[]
 ) => { [p: string]: Coin } = (
-  input: { [p: string]: Coin },
-  allCoins: string[]
+    input: { [p: string]: Coin },
+    allCoins: string[]
 ) => {
   if (!allCoins.length) {
     return input;
   }
   const cs = Object.assign({}, input);
   Object.keys(cs)
-    .filter((c) => !allCoins.includes(c))
-    .forEach((c) => delete cs[c]);
+      .filter((c) => !allCoins.includes(c))
+      .forEach((c) => delete cs[c]);
 
   return cs;
 };
 
-/*export const calculateCoinsByTicker: (
+/* export const calculateCoinsByTicker: (
   res: ICryptoSnapshotTickers
 ) => { [p: string]: Coin } | undefined = (res: ICryptoSnapshotTickers) => {
   return res.tickers
@@ -108,7 +108,7 @@ export const filterCoins: (
 };*/
 
 export const calculateCoinsByWazirXAndCoinCap = (
-  getCoins: IWazirXSnapshotMetaData
+    getCoins: IWazirXSnapshotMetaData
 ) => {
   return getCoins.tickers
     ?.map((t: IWazirXSnapshotTickers) => {
@@ -122,7 +122,7 @@ export const calculateCoinsByWazirXAndCoinCap = (
     .filter((t: IWazirXSnapshotTickers) => t)
     .reduce((total: any, current: any) => {
       if (current) {
-        const { symbol, price } = current;
+        const {symbol, price} = current;
         const data = (
           coinList as unknown as {
             [key: string]: {
@@ -133,9 +133,9 @@ export const calculateCoinsByWazirXAndCoinCap = (
             };
           }
         )[symbol];
-        const { id, name } = data || {};
+        const {id, name} = data || {};
         if (id) {
-          total[symbol] = { id, price, symbol, name };
+          total[symbol] = {id, price, symbol, name};
         }
       }
       return total;
@@ -166,8 +166,8 @@ export const fetchCoinsFromCoinCapAndWazirX = async () => {
   let getMappedCoinCapCoins: any;
   let getMappedWazirXCoins: any;
   const getAllCoinFromCoinCap = await axios.get(
-    coinCapAPIEndPoint,
-    defaultHeader
+      coinCapAPIEndPoint,
+      defaultHeader
   );
   if (
     getAllCoinFromCoinCap &&
@@ -175,16 +175,16 @@ export const fetchCoinsFromCoinCapAndWazirX = async () => {
     getAllCoinFromCoinCap.data.data.length
   ) {
     getMappedCoinCapCoins = await getAllCoinFromCoinCap.data.data.map(
-      (getCoins: any, index: number) => ({
-        name: getCoins.id.toUpperCase(),
-        symbol: getCoins.symbol.toUpperCase(),
-        price: getCoins.rateUsd,
-      })
+        (getCoins: any, index: number) => ({
+          name: getCoins.id.toUpperCase(),
+          symbol: getCoins.symbol.toUpperCase(),
+          price: getCoins.rateUsd,
+        })
     );
   }
   const getAllCoinFromWazirX = await axios.get(
-    wazirXAPIEndPoint,
-    defaultHeader
+      wazirXAPIEndPoint,
+      defaultHeader
   );
   if (
     getAllCoinFromWazirX &&
@@ -192,11 +192,11 @@ export const fetchCoinsFromCoinCapAndWazirX = async () => {
     getAllCoinFromWazirX.data.length
   ) {
     getMappedWazirXCoins = await getAllCoinFromWazirX.data.map(
-      (getCoins: any, index: number) => ({
-        name: getCoins.symbol.toUpperCase(),
-        symbol: getCoins.baseAsset.toUpperCase(),
-        price: getCoins.lastPrice,
-      })
+        (getCoins: any, index: number) => ({
+          name: getCoins.symbol.toUpperCase(),
+          symbol: getCoins.baseAsset.toUpperCase(),
+          price: getCoins.lastPrice,
+        })
     );
   }
   const mergedMappedCoins = [
@@ -220,9 +220,9 @@ export const fetchCoins = async () => {
     if (newCoins) {
       const allCoins = await getAllCoins();
       await firestore()
-        .collection("stats")
-        .doc("coins")
-        .set(filterCoins(newCoins, allCoins), { merge: true });
+          .collection("stats")
+          .doc("coins")
+          .set(filterCoins(newCoins, allCoins), {merge: true});
     }
   }
 };
@@ -240,29 +240,29 @@ export const updatePriceArray = async (before: any, after: any) => {
       }
       after[key].last24HoursPrice = newPriceArray;
       const trend = Number(
-        Number(
-          ((after[key].price || 0) /
+          Number(
+              ((after[key].price || 0) /
             (newPriceArray[newPriceArray.length - 1] || 1) -
             1) *
             100
-        ).toFixed(3)
+          ).toFixed(3)
       );
       after[key].trend = trend;
     }
   }
 
   await firestore()
-    .collection("stats")
-    .doc("coins")
-    .set(after, { merge: true });
+      .collection("stats")
+      .doc("coins")
+      .set(after, {merge: true});
 };
 
 export const sequence_operate = (
-  arr: (() => void)[],
-  index: number,
-  interval: number | undefined
+    arr: (() => void)[],
+    index: number,
+    interval: number | undefined
 ) => {
-  setTimeout(function () {
+  setTimeout(function() {
     try {
       arr[index] && arr[index]();
     } catch (e) {
@@ -273,17 +273,17 @@ export const sequence_operate = (
 };
 
 async function saveCPVI(value: CPVIObj) {
-  const { timestamp, coin } = value;
+  const {timestamp, coin} = value;
   console.log("coin --->", coin);
   console.log("value --->", value);
   value.direction0 = Math.trunc(value.direction0);
   value.direction1 = Math.trunc(value.direction1);
   await firestore()
-    .collection("askBidStats")
-    .doc("results")
-    .collection(coin)
-    .doc(timestamp.toMillis() + "")
-    .set(value, { merge: true });
+      .collection("askBidStats")
+      .doc("results")
+      .collection(coin)
+      .doc(timestamp.toMillis() + "")
+      .set(value, {merge: true});
 }
 
 export const fetchAskBidCoin = async () => {
@@ -309,14 +309,14 @@ export const fetchAskBidCoin = async () => {
 export const fetchAskBidPair = async () => {
   await cpviTaskPair(async (result) => {
     for (const value of Object.values(result)) {
-      const { timestamp, coin, direction0, direction1 } = value;
+      const {timestamp, coin, direction0, direction1} = value;
       if (direction0 > 0 || direction1 > 0) {
         await firestore()
-          .collection("askBidStats")
-          .doc("results")
-          .collection(coin)
-          .doc(timestamp.toMillis() + "")
-          .set(value, { merge: true });
+            .collection("askBidStats")
+            .doc("results")
+            .collection(coin)
+            .doc(timestamp.toMillis() + "")
+            .set(value, {merge: true});
       }
     }
   });
@@ -324,9 +324,9 @@ export const fetchAskBidPair = async () => {
 
 export const prepareCPVI = async (hours: number, table: string) => {
   const collections = await firestore()
-    .collection("askBidStats")
-    .doc("results")
-    .listCollections();
+      .collection("askBidStats")
+      .doc("results")
+      .listCollections();
 
   const start = moment().toDate();
   const end = moment().subtract(hours, "hour").toDate();
@@ -337,46 +337,46 @@ export const prepareCPVI = async (hours: number, table: string) => {
 
     console.log("collection --->", collection.id);
     const data = await firestore()
-      .collection("askBidStats")
-      .doc("results")
-      .collection(collection.id)
-      .where("timestamp", "<", firestore.Timestamp.fromDate(start))
-      .where("timestamp", ">", firestore.Timestamp.fromDate(end))
-      .get();
+        .collection("askBidStats")
+        .doc("results")
+        .collection(collection.id)
+        .where("timestamp", "<", firestore.Timestamp.fromDate(start))
+        .where("timestamp", ">", firestore.Timestamp.fromDate(end))
+        .get();
 
     if (!data.docs.length) {
       continue;
     }
-    const { direction0, direction1 } = data.docs.reduce(
-      (total, doc) => {
-        const { direction0, direction1 } = doc.data() as CPVIObj;
-        return {
-          direction0: total.direction0 + direction0,
-          direction1: total.direction1 + direction1,
-        };
-      },
-      {
-        direction0: 0,
-        direction1: 0,
-      }
+    const {direction0, direction1} = data.docs.reduce(
+        (total, doc) => {
+          const {direction0, direction1} = doc.data() as CPVIObj;
+          return {
+            direction0: total.direction0 + direction0,
+            direction1: total.direction1 + direction1,
+          };
+        },
+        {
+          direction0: 0,
+          direction1: 0,
+        }
     );
     console.log("direction0 --->", direction0);
     console.log("direction1 --->", direction1);
     if (direction0 > 0 || direction1 > 0) {
       await firestore()
-        .collection("askBidStats")
-        .doc(table)
-        .collection(collection.id)
-        .doc(firestore.Timestamp.now().toMillis() + "")
-        .set(
-          {
-            direction0,
-            direction1,
-            coin: collection.id,
-            timestamp: firestore.FieldValue.serverTimestamp(),
-          },
-          { merge: true }
-        );
+          .collection("askBidStats")
+          .doc(table)
+          .collection(collection.id)
+          .doc(firestore.Timestamp.now().toMillis() + "")
+          .set(
+              {
+                direction0,
+                direction1,
+                coin: collection.id,
+                timestamp: firestore.FieldValue.serverTimestamp(),
+              },
+              {merge: true}
+          );
     }
   }
   // });
