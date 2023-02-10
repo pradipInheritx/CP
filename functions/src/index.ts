5ff4fecf68488e7b7eb0e33acb8519e068f2e6c6
Mukut Prasad <mukutprasad1992@gmail.com>
2023-02-10 15:25:29 +0530

    Remove time duration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -608,13 +608,11 @@ exports.onCreatePaxTransaction = functions.firestore
     }
   });
 
-exports.fetchCoins = functions.pubsub
-  .schedule("30 11 * * *")
-  .onRun(async () => {
-    [0, 30].forEach((i) => {
-      setTimeout(async () => await fetchCoins(), i * 1000);
-    });
+exports.fetchCoins = functions.pubsub.schedule("* * * * *").onRun(async () => {
+  [0, 30].forEach((i) => {
+    setTimeout(async () => await fetchCoins(), i * 1000);
   });
+});
 
 // ws.onmessage = async ({data}) => {
 //   // console.log('firebaseFunction message')
