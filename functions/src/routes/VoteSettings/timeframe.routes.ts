3f8756cd5ba2abaef7392f1659e90b8da476bd16
unknown <Mukut.Prasad@eton-solutions.com>
2023-05-08 17:00:37 +0530

    Worked on timeframe

--- functions/src/routes/VoteSettings/timeframe.routes.ts
+++ functions/src/routes/VoteSettings/timeframe.routes.ts
@@ -1,23 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
 import {
-  createTimeframe,
   getTimeframe,
-  getTimeframeById,
-  deleteTimeframeById,
   updateTimeframe,
 } from "../../common/models/Admin/VoteSettings/timeframe";
-import {auth} from "../../common/middleware/authentication";
+import { auth } from "../../common/middleware/authentication";
 
 const timeframeRouter = Router();
 
-timeframeRouter.post("/createTimeframe", auth, createTimeframe);
 timeframeRouter.get("/getTimeframe", auth, getTimeframe);
-timeframeRouter.get("/getTimeframeById/:timeFrameId", auth, getTimeframeById);
-timeframeRouter.delete(
-    "/deleteTimeframeById/:timeFrameId",
-    auth,
-    deleteTimeframeById
-);
-timeframeRouter.put("/updateTimeframe/:timeFrameId", auth, updateTimeframe);
+timeframeRouter.put("/updateTimeframe/timeFrames", auth, updateTimeframe);
 
 export default timeframeRouter;
