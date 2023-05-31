271857263a2871446ab253f876ef6823762f231c
Faizal Belim <faizal.b@inheritx.com>
2023-05-31 19:49:22 +0530

    Upload image using Busboy

--- functions/src/common/models/Admin/Rewards.ts
+++ functions/src/common/models/Admin/Rewards.ts
@@ -1,6 +1,11 @@
 import { firestore } from "firebase-admin";
+import * as admin from "firebase-admin";
+// import { uploadImage } from "../Reward";
 import { toUpper } from "lodash";
-
+const Busboy = require('busboy');
+// import os from "os";
+// import path from "path";
+import * as fs from 'fs';
 
 interface Sets {
     setName: string;
@@ -542,14 +547,134 @@ export const deleteCard = async (req: any, res: any) => {
     }
 }
 
-export const uploadFileFunc = async (req: any, res: any) => {
-    console.info("Body", req.body, "File", req.file, "Params", req.params);
-    res.status(200).send({
-        status: true,
-        message: req.body,
-        result: true,
-    });
+const createDirectory = (fileUploadPath: any) => {
+    try {
+        if (!fs.existsSync(fileUploadPath)) {
+            fs.mkdirSync(fileUploadPath, { recursive: true })
+        }
+        console.log('Folder created successfully');
+    } catch (err) {
+        console.error('Error creating folder:', err);
+    }
 }
+
+const writeFileInLocal = (req: any, imagePath: any, imageData: any) => {
+    return new Promise((resolve, reject) => {
+        console.log("Writing is running in local")
+        const filePath = imagePath + "images.jpeg";
+
+        // if (fs.existsSync(filePath)) {
+        const fileStream = fs.createWriteStream(filePath, { encoding: "utf8" });
+        req.pipe(fileStream);
+        fileStream.on('finish', () => {
+            console.log('File write successfully');
+            resolve(true);
+        });
+
+        fileStream.on('error', (err) => {
+            console.error('Error write file:', err);
+            reject(false);
+        });
+        // } else {
+        //     console.error('File does not exist.');
+        // }
+
+    })
+}
+
+export const uploadImageFunction = async (req: any, res: any) => {
+
+    const { cardId } = req.params;
+    const fields: any = {};
+    const busboy = Busboy({ headers: req.headers });
+
+    console.log("cardId -------", cardId)
+
+    busboy.on('field', (fieldname: any, val: any) => {
+        console.log(`Processed field ${fieldname}: ${val}.`);
+        fields[fieldname] = val;
+    });
+
+    const imageData: any = {};
+
+    busboy.on('file', (fieldname: any, file: any, info: any) => {
+
+        const { filename, encoding, mimeType } = info;
+        console.log("fieldname ========", fieldname)
+        console.log("file ========", file)
+        console.log("info ========", info)
+        console.log(
+            `File [${fieldname}]: filename: %j, encoding: %j, mimeType: %j`,
+            filename,
+            encoding,
+            mimeType
+        );
+        file.on('data', async (data: any) => {
+            imageData['path'] = __dirname + '/Files/'
+            imageData['data'] = data;
+
+            createDirectory(imageData.path)
+            req.setEncoding("binary");
+            writeFileInLocal(file, imageData.path, data)
+        }).on('close', () => {
+            console.log(`------------File [${fieldname}] done`);
+        });
+    });
+
+    busboy.on('finish', async () => {
+
+        const getImagePath: any = {};
+        const filePath = imageData.path + "images.jpeg";
+
+        console.log("Image path : ======", imageData.path)
+        console.log("Imagee data : ======", imageData.data)
+
+        const ref = await admin.storage().bucket("default-bucket");
+        const metaData = {
+            contentType: "Image/jpg",
+        };
+        await ref
+            .upload(filePath, metaData)
+            .then(async () => getImagePath["path"] = await getImageUrl(cardId))
+            .catch((error: any) => {
+                console.log("EROROR image", error);
+            });
+        console.log("path ====", getImagePath.path)
+        res.send({
+            result: true,
+            output: getImagePath.path
+        });
+    });
+    busboy.end(req.rawBody);
+}
+
+// get Image url and add into firestore
+const getImageUrl = async (
+    cardId: string
+) => {
+    const ref = await admin.storage().bucket("default-bucket");
+    const [, , meta] = await ref.getFiles({
+        maxResults: 1,
+    });
+    const url = meta.items
+        .filter((f: any) => f.contentType !== "text/plain")
+        .shift().mediaLink;
+    console.log("Image Url ", url);
+
+    const collectionData: any = await firestore()
+        .collection("cardsDetails")
+        .doc(cardId)
+        .get();
+    console.log("collectionData--- ", collectionData.data());
+    const cardData = collectionData.data();
+    cardData.imageUpload = url;
+
+    await firestore()
+        .collection("cardsDetails")
+        .doc(cardId)
+        .set(cardData);
+    console.log("image url -------", url)
+};
 
 export const errorLogging = async (
     funcName: string,
--- functions/src/routes/RewardNftAdmin.routes.ts
+++ functions/src/routes/RewardNftAdmin.routes.ts
@@ -1,19 +1,5 @@
 import { Router } from "express";
 import { auth } from "../common/middleware/authentication";
-// import multer from 'multer';
-import upload from "../common/helpers/fileUploadConfig"
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, '../uploads/')
-//   },
-//   filename: function (req: any, file: any, cb: any) {
-//     console.info("File", file);
-//     cb(null, file.originalname)
-//   }
-// });
-
-// const upload = multer({ storage: storage });
 
 import {
   createAlbum,
@@ -26,7 +12,7 @@ import {
   deleteAlbum,
   deleteSet,
   deleteCard,
-  uploadFileFunc
+  uploadImageFunction
 } from "../common/models/Admin/Rewards";
 
 const rewardNftAdminRouter = Router();
@@ -41,6 +27,6 @@ rewardNftAdminRouter.put("/updateCard/:cardId", auth, updateCard);
 rewardNftAdminRouter.delete("/deleteAlbum/:albumId", auth, deleteAlbum);
 rewardNftAdminRouter.delete("/deleteSet", auth, deleteSet);
 rewardNftAdminRouter.delete("/deleteCard/:cardId", auth, deleteCard);
-rewardNftAdminRouter.put("/uploadFile/:cardId", upload.single('file'), uploadFileFunc);
+rewardNftAdminRouter.post("/uploadFile/:cardId", uploadImageFunction);
 
 export default rewardNftAdminRouter;
