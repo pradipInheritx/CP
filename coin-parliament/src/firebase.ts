0c7614c898e5345b97453dc18a32630821a94577
Pradip Kher <pradip.k@inheritx.com>
2023-08-09 10:01:14 +0530

    offline firebase

--- coin-parliament/src/firebase.ts
+++ coin-parliament/src/firebase.ts
@@ -1,10 +1,11 @@
 import firebase from "firebase/compat";
-import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { CACHE_SIZE_UNLIMITED, Firestore, connectFirestoreEmulator, getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 import { getApp } from "firebase/app";
 import { connectStorageEmulator, getStorage } from "firebase/storage";
 import { getMessaging, isSupported } from "firebase/messaging";
+import { getDatabase, ref, onDisconnect } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,7 +18,7 @@ const firebaseConfig = {
   measurementId: `G-${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 export const messaging = (async () => {
   try {
@@ -36,6 +37,25 @@ export const db = getFirestore();
 export const auth = getAuth();
 export const functions = getFunctions(getApp());
 export const storage = getStorage();
+const firestore = firebase.firestore();
+firebase.database().goOffline();
+firestore.enablePersistence()
+  .catch((err) => {
+    if (err.code === 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled in one tab at a time.
+      console.error('Multiple tabs open, only one tab can enable persistence.');
+    } else if (err.code === 'unimplemented') {
+      // The current browser does not support all of the features required to enable persistence.
+      console.error('Browser does not support enabling persistence.');
+    }
+  });
+
+
+const data = getDatabase();
+
+const presenceRef = ref(data, "disconnectmessage");
+// Write a string when this client loses connection
+onDisconnect(presenceRef).set("I disconnected!");
 // if (window.location.hostname === "localhost") {
 //   connectFirestoreEmulator(db, "localhost", 8080);
 //   connectAuthEmulator(auth, "http://localhost:9099");
