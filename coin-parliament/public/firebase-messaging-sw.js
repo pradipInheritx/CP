5aaf5af1863a7d73454552eb96e9d32cbbb8b61d
Pradip Kher <pradip.k@inheritx.com>
2023-08-22 16:06:13 +0530

    .

--- coin-parliament/public/firebase-messaging-sw.js
+++ coin-parliament/public/firebase-messaging-sw.js
@@ -30,59 +30,6 @@ self.addEventListener('activate', (event) => {
         })
     );
 });
-
-
-// this.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME).then((cache) => {
-//             urls = [
-//                 '/static/js/bundle.js',
-//                 '/static/js/main.chunk.js',
-//                 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css',
-//                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css',
-//                 'https://s3.tradingview.com/tv.js',
-//                 'https://bridgeapp-dev.welldapp.com/widget/wldp-widget.js?application=votetoearn&init=true&autoconnect=false&visible=true',
-//                 '/favicon.ico',
-//                 '/manifest.json',
-//                 'https://bridgeapp-dev.welldapp.com/assets/data/netwoks-rpc.json',
-//                 '/static/js/vendors~main.chunk.js',
-//                 'https://api.coingecko.com/api/v3/coins/ethereum',
-//                 'https://bridgeapp-dev.welldapp.com/assets/data/networks-names.json',
-//                 'https://bridgeapp-dev.welldapp.com/assets/data/tokens.json',
-//                 '/firebase-messaging-sw.js',
-//                 '/android-chrome-192x192.png',
-//                 '/images/no_logo.png',
-//                 '/index.html',
-//                 '/',
-//                 '/homescreem.png',
-//                 OFFLINE_URL
-//             ]
-//             cache.addAll(urls);
-//         }).catch(() => { })
-//     )
-// });
-
-// this.addEventListener('fetch', (event) => {
-
-//     // event.waitUntil(this.registration.showNotification('Hello', {
-//     //     body: 'Body',
-//     //     click_action: "http://localhost:3000"
-//     // }))
-//     if (!navigator.onLine) {
-//         event.respondWith(
-//             caches.match(event.request).then((res) => {
-//                 if (res) {
-//                     return res;
-//                 }
-//                 let requestUrl = event.request.clone();
-//                 fetch(requestUrl);
-//             }).catch(() => caches.match(OFFLINE_URL))
-//         )
-//     }
-// });
-
-
-
 importScripts("https://www.gstatic.com/firebasejs/4.13.0/firebase-app.js");
 importScripts(
     "https://www.gstatic.com/firebasejs/4.13.0/firebase-messaging.js"
--- coin-parliament/src/firebase.ts
+++ coin-parliament/src/firebase.ts
@@ -1,11 +1,10 @@
 import firebase from "firebase/compat";
-import { CACHE_SIZE_UNLIMITED, Firestore, connectFirestoreEmulator, getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 import { getApp } from "firebase/app";
 import { connectStorageEmulator, getStorage } from "firebase/storage";
 import { getMessaging, isSupported } from "firebase/messaging";
-import { getDatabase, ref, onDisconnect } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -18,11 +17,12 @@ const firebaseConfig = {
   measurementId: `G-${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 export const messaging = (async () => {
   try {
     const isSupportedBrowser = await isSupported();
+    console.log(isSupportedBrowser, 'token');
     if (isSupportedBrowser) {
       return getMessaging();
     }
@@ -37,25 +37,6 @@ export const db = getFirestore();
 export const auth = getAuth();
 export const functions = getFunctions(getApp());
 export const storage = getStorage();
-const firestore = firebase.firestore();
-firebase.database().goOffline();
-firestore.enablePersistence()
-  .catch((err) => {
-    if (err.code === 'failed-precondition') {
-      // Multiple tabs open, persistence can only be enabled in one tab at a time.
-      console.error('Multiple tabs open, only one tab can enable persistence.');
-    } else if (err.code === 'unimplemented') {
-      // The current browser does not support all of the features required to enable persistence.
-      console.error('Browser does not support enabling persistence.');
-    }
-  });
-
-
-const data = getDatabase();
-
-const presenceRef = ref(data, "disconnectmessage");
-// Write a string when this client loses connection
-onDisconnect(presenceRef).set("I disconnected!");
 // if (window.location.hostname === "localhost") {
 //   connectFirestoreEmulator(db, "localhost", 8080);
 //   connectAuthEmulator(auth, "http://localhost:9099");
