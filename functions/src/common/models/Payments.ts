c0d14ab63fa0ab23de96fd9383ff7c59d6ad4dcb
unknown <Mukut.Prasad@eton-solutions.com>
2023-09-06 19:42:33 +0530

    Remove BE call of payment module

--- functions/src/common/models/Payments.ts
+++ functions/src/common/models/Payments.ts
@@ -29,50 +29,23 @@ export const getUserWalletBalance = async (req: any, res: any) => {
 export const makePayment = async (req: any, res: any) => {
     const { userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails } = req.body;
     console.log(userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails)
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlX2lkIjowLCJvcmdfaWQiOjEzLCJpc3MiOiJXRUxMREFQUCIsInN1YiI6InZvdGV0b2Vhcm4iLCJhdWQiOlsiR1JPVVBTIiwiQVBQTElDQVRJT05TIiwiQVVUSCIsIldFQjMiXSwiZXhwIjoyMDIyNTkwODI1fQ.0JYa8ZLdfdtC78-DJSy91m3KqTPX9PrGMAD0rtma0_M'
-    } // Bearer token is static from WellDApp
+    await storeInDBOfPayment({ userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails })
 
-    const getResponseAfterTransaction = await axios.post("https://console.dev.welldapp.io/api/transactions", {
-        "callback_secret": "",
-        "callback_url": "",
-        "method": "getTransaction",
-        "params": {
-            "amount": amount,
-            "network": network,
-            "origincurrency": origincurrency,
-            "token": token
-        },
-        "user": userEmail
-    }, { headers: headers })
-
-
-    if (getResponseAfterTransaction && getResponseAfterTransaction.status === 201) {
-        await storeInDBOfPayment({ userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails }, getResponseAfterTransaction.data)
-
-        res.status(200).json({
-            status: true,
-            message: `Payment done successfully of amount ${amount}$`,
-            data: getResponseAfterTransaction.data
-        })
-    } else {
-        res.status(400).json({
-            status: false,
-            message: "Something went wrong while making the payment",
-            data: {}
-        })
-    }
+    res.status(200).json({
+        status: true,
+        message: `Payment done successfully of amount ${amount}$`,
+        data: { userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails }
+    })
 }
 
-export const storeInDBOfPayment = async (metaData: any, response: any) => {
+export const storeInDBOfPayment = async (metaData: any) => {
     if (metaData.transactionType === "UPGRADE" && metaData?.userId) {
         await addIsUpgradedValue(metaData.userId)
     }
     if (metaData.transactionType === "EXTRAVOTES") {
         await addIsExtraVotePurchase(metaData)
     }
-    await firestore().collection("payments").add({ ...metaData, ...response, timestamp: firestore.FieldValue.serverTimestamp() })
+    await firestore().collection("payments").add({ ...metaData, timestamp: firestore.FieldValue.serverTimestamp() })
 }
 
 const addIsExtraVotePurchase = async (metaData: any) => {
