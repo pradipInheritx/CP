05e11560edd2b2d722ea934b0be94c3c706be1a0
Pradip Kher <pradip.k@inheritx.com>
2023-09-15 14:49:14 +0530

    card UI changes

--- coin-parliament/src/Pages/NftOneCard.tsx
+++ coin-parliament/src/Pages/NftOneCard.tsx
@@ -327,7 +327,7 @@ const NftOneCard = ({ darkTheme = false, DivClass, HeaderText, HeaderClass, widt
               </div>
               <div>
                 {/* <span className="epic_text">&nbsp; Epic &nbsp; </span><br /> */}
-                <span className='cardname' style={{
+                <span className='cardname pb-2' style={{
                   color: (darkTheme ? "white" : ''),
                 }}
                 >
@@ -335,6 +335,14 @@ const NftOneCard = ({ darkTheme = false, DivClass, HeaderText, HeaderClass, widt
 
 
                 </span>
+                <p
+                  style={{
+                    color: "black",
+                    textAlign: 'center'
+                  }}
+                >
+                  {fulldata?.albumName?.toLocaleUpperCase()}
+                </p>
               </div>
               {/* <br /> */}
               <div className='card-body'
@@ -359,8 +367,8 @@ const NftOneCard = ({ darkTheme = false, DivClass, HeaderText, HeaderClass, widt
                   :
                   <div
                     style={{
-                      // width: "240px",
-                      // height: "220px",
+                      width: "240px",
+                      height: "220px",
                       // border:"1px solid red",
                       // overflow:"hidden"
                       position: "relative",
@@ -371,7 +379,7 @@ const NftOneCard = ({ darkTheme = false, DivClass, HeaderText, HeaderClass, widt
                       className=""
                       style={{
                         position: "absolute",
-                        left: "-30px",
+                        left: "-50px",
                         bottom: "60px",
                         transform: "rotate(-90deg)",
                         width: "100px",
@@ -400,16 +408,6 @@ const NftOneCard = ({ darkTheme = false, DivClass, HeaderText, HeaderClass, widt
                         // marginTop: "-10px",
                       }}
                     />
-                    <p
-                      style={{
-                        position: "absolute",
-                        // left: "-20px",
-                        bottom: "-14PX",
-                        // transform: "rotate(-90deg)",
-                        color: "black",
-
-                      }}
-                    >{fulldata?.albumName?.toLocaleUpperCase()}</p>
                   </div>
                 }
 
@@ -474,7 +472,7 @@ const NftOneCard = ({ darkTheme = false, DivClass, HeaderText, HeaderClass, widt
                   >
                     {/* <p>Your CMP count</p> */}
                     <p className="mt-1 text-end lh-base">
-                        As the original Collectible card holder, you get 50% of the royalties from the fee charged on Collectible card sale lifetime! This arrangement ensures you have a continuous and substantial income stream from your digital assets. only members who upgrade their account will have their cards converted to Collectible cards
+                      As the original Collectible card holder, you get 50% of the royalties from the fee charged on Collectible card sale lifetime! This arrangement ensures you have a continuous and substantial income stream from your digital assets. only members who upgrade their account will have their cards converted to Collectible cards
                     </p>
                     {/* <p className="mt-3 text-end lh-base">
                   Watch your CMP grow with every influential vote, unlocking Parliament Coins, extra votes, and exclusive cards at key milestones. 
