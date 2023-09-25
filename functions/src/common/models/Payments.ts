70ba87676c441499d1399b456dd385f4c105b79e
unknown <Mukut.Prasad@eton-solutions.com>
2023-09-25 10:22:34 +0530

    Modify payment module API

--- functions/src/common/models/Payments.ts
+++ functions/src/common/models/Payments.ts
@@ -2,6 +2,94 @@ import axios from 'axios';
 import { firestore } from "firebase-admin";
 import { log } from 'firebase-functions/logger';
 
+export const renderPaymentPage = async (req: any, res: any) => {
+    try {
+        console.info("Come Here");
+        res.setHeader('Content-Type', 'text/html');
+        const htmlContent = `<!DOCTYPE html>
+            <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Vote2Earn demo</title>
+            <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
+
+        </head>
+    <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Step 1</h2>
+                <input type="text" id="account" placeholder="No wallet connected" disabled > 
+                <button class="primary" onclick="connectionWallet()">connectionWallet</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Step 2</h2>
+                <input id="uid" type="text" placeholder="UserID">
+                <button class="primary" onclick="send_uid()">send_uid</button>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Step 3</h2>
+                <input type="number" value="0.0001" id="amount">
+                <button class="primary" onclick="getTransaction()">getTransaction</button>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function send_uid(){
+            const uid = document.getElementById("uid").value;
+            wldp.send_uid(uid);
+        }
+
+        function connectionWallet(){
+            wldp.connectionWallet('connect','ethereum');
+        }
+
+        function getTransaction(){
+            const obj = {
+                "userId": "deGaSUcF5KYkQARHXLmQuHLjXyF3",
+                "userEmail": "mukut@inheritx.com",
+                "walletType": "Metamask",
+                "amount": 0.0001,
+                "network": "5",
+                "origincurrency": "eth",
+                "token": "ETH",
+                "transactionType": "EXTRAVOTES",
+                "numberOfVotes": 10,
+                "paymentDetails": {}
+            }
+            fetch('/coin-parliament-staging/us-central1/api/v1/payment/makePayment/toServer',{
+                method: 'POST',
+                headers: {'content-type':'application/json'},
+                body: JSON.stringify(obj)
+            })
+        }
+
+        document.addEventListener('wldp:acountChange', e => {
+           document.getElementById('account').value = e.detail.account
+        })
+
+    </script>
+
+    
+    <script  src='https://bridgeapp-dev.welldapp.com/widget/wldp-widget.js?application=app1.app&init=true&autoconnect=false&visible=true'></script>
+</body>
+</html>`;
+
+        // Send the HTML content as the response body
+        res.end(htmlContent);
+    } catch (error) {
+        console.info("error", error)
+    }
+}
+
 export const getUserWalletBalance = async (req: any, res: any) => {
     const { address, blockchain, token } = req.params;
     console.log(address, blockchain, token)
@@ -29,16 +117,14 @@ export const getUserWalletBalance = async (req: any, res: any) => {
 
 export const makePaymentToServer = async (req: any, res: any) => {
     try {
-        const headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlX2lkIjowLCJvcmdfaWQiOjEzLCJpc3MiOiJXRUxMREFQUCIsInN1YiI6InZvdGV0b2Vhcm4iLCJhdWQiOlsiR1JPVVBTIiwiQVBQTElDQVRJT05TIiwiQVVUSCIsIldFQjMiXSwiZXhwIjoyMDIyNTkwODI1fQ.0JYa8ZLdfdtC78-DJSy91m3KqTPX9PrGMAD0rtma0_M'
-        } // Bearer token is static from WellDApp
+        // const headers = {
+        //     'Content-Type': 'application/json',
+        //     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlX2lkIjowLCJvcmdfaWQiOjEzLCJpc3MiOiJXRUxMREFQUCIsInN1YiI6InZvdGV0b2Vhcm4iLCJhdWQiOlsiR1JPVVBTIiwiQVBQTElDQVRJT05TIiwiQVVUSCIsIldFQjMiXSwiZXhwIjoyMDIyNTkwODI1fQ.0JYa8ZLdfdtC78-DJSy91m3KqTPX9PrGMAD0rtma0_M'
+        // } // Bearer token is static from WellDApp
 
-        const { userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails } = req.body;
-
+        //const { userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes } = req.body;
+        const { userEmail, amount, network, origincurrency, token } = req.body;
         let requestBody = {
-            "callback_secret": "",
-            "callback_url": "",
             "method": "getTransaction",
             "params": {
                 "amount": amount,
@@ -49,19 +135,60 @@ export const makePaymentToServer = async (req: any, res: any) => {
             "user": userEmail
         }
 
-        const getDataAfterWellDApp = await axios.post(`https://console.dev.welldapp.io/api/transactions`, requestBody, { headers: headers })
+        // fetch('https://console.dev.welldapp.io/api/transactions', {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json',
+        //         'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlX2lkIjowLCJvcmdfaWQiOjEzLCJpc3MiOiJXRUxMREFQUCIsInN1YiI6InZvdGV0b2Vhcm4iLCJhdWQiOlsiR1JPVVBTIiwiQVBQTElDQVRJT05TIiwiQVVUSCIsIldFQjMiXSwiZXhwIjoyMDIyNTkwODI1fQ.0JYa8ZLdfdtC78-DJSy91m3KqTPX9PrGMAD0rtma0_M'
+        //     },
+        //     body: JSON.stringify(requestBody)
+        // })
+        //     .then(res => {
+        //         if (res.ok)
+        //             return res.json()
+        //         else
+        //             throw Error(`code ${res.status}`)
+        //     })
+        //     .then(async data => {
+        //         await storeInDBOfPayment({ userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails: {} })
+        //         await isParentExistAndGetReferalAmount(req.body);
+        //         res.json(data)
+        //     })
+        //     .catch(err => {
+        //         console.error(err)
+        //         res.status(400).send(err)
+        //     })
 
-        console.info("getDataAfterWellDApp", getDataAfterWellDApp, getDataAfterWellDApp.data)
-
-        console.log(userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails)
-
-        await storeInDBOfPayment({ userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails: getDataAfterWellDApp.data })
-        await isParentExistAndGetReferalAmount(req.body);
-        res.status(200).json({
-            status: true,
-            message: `Payment done successfully of amount ${amount}$ on the server`,
-            data: { userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails: getDataAfterWellDApp.data }
+        axios.post('https://console.dev.welldapp.io/api/transactions', requestBody,
+            {
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlX2lkIjowLCJvcmdfaWQiOjIsImlzcyI6IldFTExEQVBQIiwic3ViIjoiYXBwMS5hcHAiLCJhdWQiOlsiR1JPVVBTIiwiQVBQTElDQVRJT05TIiwiQVVUSCIsIldFQjMiXSwiZXhwIjoyMjk4MjE5MzE2fQ.XzOIhftGzwPC5F0T-xbnpWJnY5xSTmpE36648pPQwUQ'
+                }
+            }
+        ).then((res: any) => {
+            if (res.ok)
+                return res.json()
+            else
+                throw Error(`code ${res.status}`)
+        }).then((data: any) => {
+            res.json(data)
+        }).catch((err: any) => {
+            console.error(err)
+            res.status(400).send(err)
         })
+
+
+        //console.info("getDataAfterWellDApp", getDataAfterWellDApp, getDataAfterWellDApp.data)
+
+        //console.log(userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails)
+
+
+        // res.status(200).json({
+        //     status: true,
+        //     message: `Payment done successfully of amount ${amount}$ on the server`,
+        //     data: { userId, userEmail, walletType, amount, network, origincurrency, token, transactionType, numberOfVotes, paymentDetails: getDataAfterWellDApp.data }
+        // })
     } catch (error: any) {
         console.info("Error while make payment to welld app server", error)
     }
@@ -220,49 +347,48 @@ export const getTransactionHistory = async (req: any, res: any) => {
             result: error,
         });
     }
-
 }
 
-const isParentExistAndGetReferalAmount = async (data: any) => {
-    try {
-        const { userId, amount } = data;
-        console.log("userId amount ", userId, amount)
-        const getUserDetails: any = (await firestore().collection('users').doc(userId).get()).data();
-        console.log("getUserDetails : ", getUserDetails);
+// const isParentExistAndGetReferalAmount = async (data: any) => {
+//     try {
+//         const { userId, amount } = data;
+//         console.log("userId amount ", userId, amount)
+//         const getUserDetails: any = (await firestore().collection('users').doc(userId).get()).data();
+//         console.log("getUserDetails : ", getUserDetails);
 
-        if (!getUserDetails.parent) {
-            return {
-                status: false,
-                message: "Parent not available"
-            }
-        };
+//         if (!getUserDetails.parent) {
+//             return {
+//                 status: false,
+//                 message: "Parent not available"
+//             }
+//         };
 
-        const halfAmount: number = (parseFloat(amount) * 50) / 100;
+//         const halfAmount: number = (parseFloat(amount) * 50) / 100;
 
-        const finalData = {
-            parentUserId: getUserDetails?.parent,
-            childUserId: userId,
-            amount: halfAmount,
-            type: "REFERAL",
-            address: "",
-            status: "PENDING"
-        }
-        await firestore().collection('parentPayment').add(finalData).then(() => {
-            console.log("parentPayment entry is done.");
-        }).catch((error) => console.error("parentPayment entry have Error : ", error));
+//         const finalData = {
+//             parentUserId: getUserDetails?.parent,
+//             childUserId: userId,
+//             amount: halfAmount,
+//             type: "REFERAL",
+//             address: "",
+//             status: "PENDING"
+//         }
+//         await firestore().collection('parentPayment').add(finalData).then(() => {
+//             console.log("parentPayment entry is done.");
+//         }).catch((error) => console.error("parentPayment entry have Error : ", error));
 
-        return {
-            status: true,
-            message: "Parent payment initiated successfully"
-        }
+//         return {
+//             status: true,
+//             message: "Parent payment initiated successfully"
+//         }
 
-    } catch (error) {
-        return {
-            status: false,
-            message: "Something went wrong while getting the parent referal"
-        }
-    }
-}
+//     } catch (error) {
+//         return {
+//             status: false,
+//             message: "Something went wrong while getting the parent referal"
+//         }
+//     }
+// }
 
 
 export const errorLogging = async (
--- functions/src/routes/Payments.routes.ts
+++ functions/src/routes/Payments.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
-
 import {
     makePayment,
     getUserWalletBalance,
     isUserUpgraded,
     getTransactionHistory,
     makePaymentToServer,
-    getParentPayment
+    getParentPayment,
+    renderPaymentPage
 } from "../common/models/Payments";
 
 const PaymentRouter = Router();
@@ -15,12 +15,10 @@ const PaymentRouter = Router();
 PaymentRouter.post("/makePayment", makePayment);
 PaymentRouter.post("/makePayment/toServer", makePaymentToServer);
 
-
 PaymentRouter.get("/balance/:address/:blockchain/:token", getUserWalletBalance);
 PaymentRouter.get("/isUserUpgraded/:userId", isUserUpgraded);
 PaymentRouter.get("/getTransactionHistory/:userId", getTransactionHistory);
 PaymentRouter.get("/getParentPayment/:userId", getParentPayment);
-
-
+PaymentRouter.get("/renderPaymentPage", renderPaymentPage)
 
 export default PaymentRouter;
