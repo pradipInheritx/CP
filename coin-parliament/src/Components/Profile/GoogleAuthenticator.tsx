e3546d6098fd9b1e3e3dcf3da6fa12576715f00f
Manish Soni <manish.soni@brsoftech.org>
2023-10-17 16:24:07 +0530

    change api url

--- coin-parliament/src/Components/Profile/GoogleAuthenticator.tsx
+++ coin-parliament/src/Components/Profile/GoogleAuthenticator.tsx
@@ -13,6 +13,7 @@ import { getAuth, updateProfile } from "firebase/auth";
 import infobtn from '../../assets/images/info-btn.png'
 import AppStore from '../../assets/images/AppStore.png'
 import GooglePlay from '../../assets/images/GooglePlay.png'
+// import BigLogo from "../../assets/svg/logoiconx2.png";
 import {
   multiFactor,
   PhoneAuthProvider,
@@ -22,7 +23,7 @@ import {
 import { texts } from "../LoginComponent/texts";
 import axios from "axios";
 import QRCode from "qrcode";
-// import BigLogo from "../../assets/svg/logoiconx2.png";
+import BigLogo from "../../assets/svg/spblue.svg";
 import { generateGoogle2faUrl, otpurl } from "../../common/consts/contents";
 const BtnLabel = styled(Form.Check.Label)`
   ${InputAndButton}
@@ -73,7 +74,6 @@ const GoogleAuthenticator = () => {
   const auth = getAuth();
   const [copied, setCopied] = useState(false);
 
-  console.log(userInfo, 'pkkkk');
 
   const createPost = async (id: string) => {
     // @ts-ignore
@@ -88,12 +88,14 @@ const GoogleAuthenticator = () => {
       );
       return
     }
-    const data = {
+    const AllData = {
       userId: id,
       userType: "USER",
     };
+
+
     try {
-      const response = await axios.post(generateGoogle2faUrl, { data: data });
+      const response = await axios.post(generateGoogle2faUrl, { data: AllData });
       console.log(response.data);
       setSecretKey(response.data.result.base32);
       QRCode.toDataURL(response.data.result.otpauth_url, { color: { dark: "#7565f7", light: "#ffffff" } }
@@ -131,6 +133,7 @@ const GoogleAuthenticator = () => {
   };
 
   // console.log('user',userInfo,u)
+  console.log(userInfo, "!userInfo?.mfa")
   useEffect(() => {
     if (!userInfo?.mfa) createPost(userInfo?.uid as string);
     return () => setCopied(false);
@@ -281,7 +284,7 @@ const GoogleAuthenticator = () => {
                               <li>Scan the QR code or type in the code manually on your mobile device</li>
                               <li>Write down or save the secret code in case you loss your device.</li>
                               <li>Do not ever share your secret code with anyone. We will never ask for your secret code.</li>
-                              {/* <li>Do not post or share your password or send your password to others by email.</li >  */}
+                              {/* <li>Do not post or share your password or send your password to others by email.</li> */}
                             </ul>
                           </div>
                         </div>
@@ -319,7 +322,7 @@ const GoogleAuthenticator = () => {
                             >
 
                               <img
-                                src={'/VTE logo.png'}
+                                src={BigLogo}
                                 alt="QR code for Google Authenticator"
                                 style={{ maxWidth: "100px", position: 'absolute', top: '35%' }}
                               />
