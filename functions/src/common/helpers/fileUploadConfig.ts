ca5426cca1bce3cbd340aec5b5bd9c60fcc201b9
unknown <Mukut.Prasad@eton-solutions.com>
2023-11-23 14:55:11 +0530

    Resolve issue

--- functions/src/common/helpers/fileUploadConfig.ts
+++ functions/src/common/helpers/fileUploadConfig.ts
@@ -1,179 +1,170 @@
 import * as admin from "firebase-admin";
 import env from "../../env/env.json";
 import Busboy from "busboy";
-
 import {
-  getAlbumDetails,
-  getCardDetails,
-  updateFileLink,
-  errorLogging,
+    getAlbumDetails,
+    getCardDetails,
+    updateFileLink,
+    errorLogging,
 } from "../models/Admin/Rewards";
 
 export const imageUploadFunction = async (req: any, res: any) => {
-  const { fileType, forModule, id } = req.params;
+    const { fileType, forModule, id } = req.params;
 
-  try {
-    const checkId =
-      forModule.toUpperCase() === "CARD"
-        ? await getCardDetails(id)
-        : forModule.toUpperCase() === "ALBUM"
-        ? await getAlbumDetails(id)
-        : { albumName: "", cardName: "", message: true };
-    console.log("check Id ------", checkId);
-    if (checkId?.message) {
-      return res.status(400).send({
-        status: false,
-        message: `only ${"CARD"} and ${"ALBUM"} are allowed`,
-        result: null,
-      });
-    } else if (!checkId?.albumName && !checkId?.cardName) {
-      return res.status(404).send({
-        status: false,
-        message: `This ID does not exist ${id} in ${forModule}`,
-        result: null,
-      });
+    try {
+        const checkId = forModule.toUpperCase() === "CARD" ? await getCardDetails(id) : forModule.toUpperCase() === "ALBUM" ? await getAlbumDetails(id) : { albumName: "", cardName: "", message: true };
+        if (checkId?.message) {
+            return res.status(400).send({
+                status: false,
+                message: `only ${"CARD"} and ${"ALBUM"} are allowed`,
+                result: null,
+            });
+        } else if (!checkId?.albumName && !checkId?.cardName) {
+            return res.status(404).send({
+                status: false,
+                message: `This ID does not exist ${id} in ${forModule}`,
+                result: null,
+            });
+        }
+        const busboy = Busboy({ headers: req.headers });
+        const bucket = admin
+            .storage()
+            .bucket(env.STORAGE_BUCKET_URL);
+
+        //On File Event
+        busboy.on("file", (fieldname: any, file: any, fileMeta: any) => {
+            const fileUpload = bucket.file(
+                `${checkId.albumName}/${fileMeta.filename}`
+            );
+            const fileStream = file.pipe(
+                fileUpload.createWriteStream({
+                    metadata: {
+                        contentType: fileMeta.mimeType,
+                    },
+                })
+            );
+            fileUpload
+                .getSignedUrl({
+                    action: "read",
+                    expires: "03-09-2491",
+                })
+                .then(async (signedUrls) => {
+                    console.log("Public Url ------\n", signedUrls[0]);
+                    const result = await updateFileLink(
+                        forModule,
+                        fileType,
+                        id,
+                        signedUrls[0]
+                    );
+                    return res.status(200).send({
+                        status: true,
+                        message: "Media uploaded and card image url updated successfully",
+                        result: { id, ...result },
+                    });
+                });
+
+            //On Error Event
+            fileStream.on("error", (error: any) => {
+                console.error("File Upload Error Event:", error);
+                return res.status(500).send({
+                    status: true,
+                    message: "Unable to upload file",
+                    result: null,
+                });
+            });
+        });
+
+        // On Finish Event
+        busboy.on("finish", async () => { });
+        busboy.end(req.rawBody);
+    } catch (error) {
+        errorLogging("uploadFiles", "ERROR", error);
+        res.status(500).send({
+            status: false,
+            message: "Something went wrong in server",
+            result: error,
+        });
     }
-
-    const busboy = Busboy({ headers: req.headers });
-
-    const bucket = admin
-      .storage()
-      .bucket(env.STORAGE_BUCKET_URL);
-
-    //On File Event
-    busboy.on("file", (fieldname: any, file: any, fileMeta: any) => {
-      const fileUpload = bucket.file(
-        `${checkId.albumName}/${fileMeta.filename}`
-      );
-      const fileStream = file.pipe(
-        fileUpload.createWriteStream({
-          metadata: {
-            contentType: fileMeta.mimeType,
-          },
-        })
-      );
-      fileUpload
-        .getSignedUrl({
-          action: "read",
-          expires: "03-09-2491",
-        })
-        .then(async (signedUrls) => {
-          console.log("Public Url ------\n", signedUrls[0]);
-          const result = await updateFileLink(
-            forModule,
-            fileType,
-            id,
-            signedUrls[0]
-          );
-          return res.status(200).send({
-            status: true,
-            message: "Media uploaded and card image url updated successfully",
-            result: { id, ...result },
-          });
-        });
-
-      //On Error Event
-      fileStream.on("error", (error: any) => {
-        console.error("File Upload Error Event:", error);
-        return res.status(500).send({
-          status: true,
-          message: "Unable to upload file",
-          result: null,
-        });
-      });
-    });
-
-    // On Finish Event
-    busboy.on("finish", async () => {});
-    busboy.end(req.rawBody);
-  } catch (error) {
-    errorLogging("uploadFiles", "ERROR", error);
-    res.status(500).send({
-      status: false,
-      message: "Something went wrong in server",
-      result: error,
-    });
-  }
 };
 
 export const avatarUploadFunction = async (req: any, res: any) => {
-  const { userId } = req.params;
-  const { bio } = req.body;
+    const { userId } = req.params;
+    const { bio } = req.body;
 
-  try {
-    // upload the user's avatar
-    const getUserName = (
-      await admin.firestore().collection("users").doc(userId).get()
-    ).data();
-    const newFileName = getUserName?.displayName + userId;
-
-    const busboy = Busboy({ headers: req.headers });
-    const bucket = admin
-      .storage()
-      .bucket(env.STORAGE_BUCKET_URL);
-
-    busboy.on("avatar", (fieldname: any, file: any, fileMeta: any) => {
-      const fileUpload = bucket.file(`UsersAvatar/${newFileName}`);
-      const fileStream = file.pipe(
-        fileUpload.createWriteStream({
-          metadata: {
-            contentType: fileMeta.mimeType,
-          },
-        })
-      );
-      fileUpload
-        .getSignedUrl({
-          action: "read",
-          expires: "03-09-2491",
-        })
-        .then(async (signedUrls) => {
-          console.log("Public Url ------\n", signedUrls[0]);
-
-          await admin
-            .firestore()
-            .collection("users")
-            .doc(userId)
-            .set({ bio, avatar: signedUrls[0] }, { merge: true });
-
-          const result = (
+    try {
+        // upload the user's avatar
+        const getUserName = (
             await admin.firestore().collection("users").doc(userId).get()
-          ).data();
-          return res.status(200).send({
-            status: true,
-            message: "update avatar and bio successfully",
-            result: { result },
-          });
+        ).data();
+        const newFileName = getUserName?.displayName + userId;
+
+        const busboy = Busboy({ headers: req.headers });
+        const bucket = admin
+            .storage()
+            .bucket(env.STORAGE_BUCKET_URL);
+
+        busboy.on("avatar", (fieldname: any, file: any, fileMeta: any) => {
+            const fileUpload = bucket.file(`UsersAvatar/${newFileName}`);
+            const fileStream = file.pipe(
+                fileUpload.createWriteStream({
+                    metadata: {
+                        contentType: fileMeta.mimeType,
+                    },
+                })
+            );
+            fileUpload
+                .getSignedUrl({
+                    action: "read",
+                    expires: "03-09-2491",
+                })
+                .then(async (signedUrls) => {
+                    console.log("Public Url ------\n", signedUrls[0]);
+
+                    await admin
+                        .firestore()
+                        .collection("users")
+                        .doc(userId)
+                        .set({ bio, avatar: signedUrls[0] }, { merge: true });
+
+                    const result = (
+                        await admin.firestore().collection("users").doc(userId).get()
+                    ).data();
+                    return res.status(200).send({
+                        status: true,
+                        message: "update avatar and bio successfully",
+                        result: { result },
+                    });
+                });
+
+            //On Error Event
+            fileStream.on("error", (error: any) => {
+                console.error("File Upload Error Event:", error);
+                return res.status(500).send({
+                    status: true,
+                    message: "Unable to upload file",
+                    result: null,
+                });
+            });
         });
 
-      //On Error Event
-      fileStream.on("error", (error: any) => {
-        console.error("File Upload Error Event:", error);
-        return res.status(500).send({
-          status: true,
-          message: "Unable to upload file",
-          result: null,
+        busboy.on("finish", async () => {
+            // const downloadURL = await bucket
+            //     .file(fileToBeUploaded.file)
+            //     .publicUrl()
+            // console.info("File Url", downloadURL);
+            // return res.status(200).send({
+            //     status: true,
+            //     message: "Image uploaded and card image url updated successfully",
+            //     result: { imageUrl: downloadURL },
+            // });
         });
-      });
-    });
-
-    busboy.on("finish", async () => {
-      // const downloadURL = await bucket
-      //     .file(fileToBeUploaded.file)
-      //     .publicUrl()
-      // console.info("File Url", downloadURL);
-      // return res.status(200).send({
-      //     status: true,
-      //     message: "Image uploaded and card image url updated successfully",
-      //     result: { imageUrl: downloadURL },
-      // });
-    });
-    busboy.end(req.rawBody);
-  } catch (error) {
-    errorLogging("uploadFiles", "ERROR", error);
-    res.status(500).send({
-      status: false,
-      message: "Something went wrong in server",
-      result: error,
-    });
-  }
+        busboy.end(req.rawBody);
+    } catch (error) {
+        errorLogging("uploadFiles", "ERROR", error);
+        res.status(500).send({
+            status: false,
+            message: "Something went wrong in server",
+            result: error,
+        });
+    }
 };
