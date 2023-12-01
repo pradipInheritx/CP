5bf1c946a726fbd1f3c4ae8806cc48985870f864
Manish Soni <manish.soni@brsoftech.org>
2023-12-01 18:28:44 +0530

    take pull

--- coin-parliament/src/Pages/Ambassador/Ambassador.tsx
+++ coin-parliament/src/Pages/Ambassador/Ambassador.tsx
@@ -43,9 +43,9 @@ const
   Ambassador = () => {
     const translate = useTranslation();
     const { showToast } = useContext(NotificationContext);
-    const { user } = useContext(UserContext);
+    const { user,userInfo } = useContext(UserContext);
     const { setLogin } = useContext(AppContext)
-    const referralUrl = `${document.location.protocol}//${document.location.host}/?refer=${user?.uid}`;
+    const referralUrl = `${document.location.protocol}//${document.location.host}/?refer=${userInfo?.displayName}`;
     const url = referralUrl
     const shareText = `Hey,%0ajoin me on Voting Parliament and earn rewards for your opinion!%0aLet's vote together!`
     const [showShare, setShowShare] = useState(false);
