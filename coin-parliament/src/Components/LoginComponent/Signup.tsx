11f12aebfe0d09025e2f47ee2d68369226c50aea
Manish Soni <manish.soni@brsoftech.org>
2023-12-08 16:36:23 +0530

    without popup

--- coin-parliament/src/Components/LoginComponent/Signup.tsx
+++ coin-parliament/src/Components/LoginComponent/Signup.tsx
@@ -75,7 +75,7 @@ const Signup = ({ setUser, setSignup, signup, authProvider }: SignupProps) => {
   const [signupLoading, setSignupLoading] = useState(false)
   let navigate = useNavigate();
   const search = useLocation().search;
-  const refer = new URLSearchParams(search).get("refer") || "user_test";
+  const refer = new URLSearchParams(search).get("refer") || "VoteToEarn";
   const [preantId, setPreantId] = useState(null)
 
   const getUserId = async () => {
