8f4aa25bdeedfedb44bddbc56e6e1a87533a9aef
unknown <Mukut.Prasad@eton-solutions.com>
2023-12-13 20:46:35 +0530

    Worked on Set Leaders API filling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -84,7 +84,6 @@ import {
   sendCustomNotificationOnSpecificUsers,
   checkUserStatusIn24hrs,
   checkInActivityOfVotesAndSendNotification,
-  TitleUpgradeNotificationLogic_Testing,
 } from "./common/models/SendCustomNotification";
 import { getCoinCurrentAndPastDataDifference } from "./common/models/Admin/Coin";
 
@@ -987,7 +986,7 @@ exports.getCPVIForVote = functions.https.onCall(async (data) => {
 
 exports.CPVIForCoin = functions.https.onCall(async (data) => {
   // console.log("getCPVIForVote(data) =>", data);
-  const {coinName} = data;
+  const { coinName } = data;
   return await CPVIForCoin(coinName);
 });
 
