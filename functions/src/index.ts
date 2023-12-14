cd2b2c86602be0809b990105824f50250934232b
Faizal Belim <faizal.b@inheritx.com>
2023-12-14 19:25:18 +0530

    add new addkeyFunction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -199,6 +199,26 @@ exports.onCreateUser = functions.auth.user().onCreate(async (user) => {
     console.log("Error while create user in stock", error);
   }
 });
+import {addNewKeysInCollection} from "./common/models/User";
+// temporarily used to add add keys to the collection
+exports.addNewKeysInCollection = functions.https.onCall((data) => {
+  const { keyName, keyValue, collectionName } = data;
+  console.log(
+    `keyName : ${keyName}, keyValue : ${keyValue}, collectionName : ${collectionName}`
+  );
+
+  if (keyName && collectionName) {
+    const result = addNewKeysInCollection(keyName, keyValue, collectionName);
+    return result;
+  } else
+    return {
+      message: "some credentials is missing",
+    };
+});
+
+
+
+
 
 const getMaxVotes = async () => {
   const getVoteAndReturnQuery = await admin.firestore().collection("settings").doc("settings").get();
