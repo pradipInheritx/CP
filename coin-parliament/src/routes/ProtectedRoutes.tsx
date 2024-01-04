2671700cc27fd20358ef7ce9121a31d7cdcc37f2
Yauvan modi <yauvanmodi.uv@gmail.com>
2024-01-04 17:34:03 +0530

    slove the problam where  we login without verified email then it is keep reloading

--- coin-parliament/src/routes/ProtectedRoutes.tsx
+++ coin-parliament/src/routes/ProtectedRoutes.tsx
@@ -9,7 +9,7 @@ const ProtectedRoutes = () => {
 
     const [checkAuth, setCheckAuth] = useState<boolean | string>('none');
     const [loading, setLoading] = useState(true);
-    const { setUser, userInfo } = useContext(UserContext);
+    const { setUser, userInfo ,setUserInfo} = useContext(UserContext);
 
     useEffect(() => {
         auth.onAuthStateChanged(function (user) {
@@ -19,12 +19,16 @@ const ProtectedRoutes = () => {
                 setUser(user);
             } else {
                 setCheckAuth(false);
+                setUserInfo();
                 setUser();
             }
             setLoading(false);
         });
 
-    }, [JSON.stringify(auth.currentUser)]);
+    }, [auth?.currentUser]);
+
+    console.log(loading , localStorage.getItem('mfa_passed'),auth?.currentUser,checkAuth,userInfo,'hello');
+    
     return (!loading && localStorage.getItem('mfa_passed') !== 'true') ? (checkAuth ? <Outlet /> : <Navigate to={'/login'} />) : <></>;
 }
 
