f38bb7909357abe3d398569a32a9dcdd8ac7d7a1
Faizal Belim <faizal.b@inheritx.com>
2024-01-08 12:20:40 +0530

    title upgrade time change

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -748,12 +748,13 @@ exports.getCoinCurrentAndPastDataDifference = functions.pubsub
   });
 
 exports.checkTitleUpgrade24Hour = functions.pubsub
-  .schedule("0 0 * * *")
+  .schedule("*/7 * * * *")
   .onRun(async () => {
     console.log("---Start checkTitleUpgrade24Hour -------");
     const date = new Date();
     const nowTime = date.getTime();
-    const yesterdayTime = nowTime - 24 * 60 * 60 * 1000;
+    // const yesterdayTime = nowTime - 24 * 60 * 60 * 1000;
+    const yesterdayTime = nowTime - 7 * 60 * 1000;
     await checkUserStatusIn24hrs(nowTime, yesterdayTime);
     await getFollowersFollowingsAndVoteCoin(nowTime, yesterdayTime);
     console.log("---End checkTitleUpgrade24Hour -------");
