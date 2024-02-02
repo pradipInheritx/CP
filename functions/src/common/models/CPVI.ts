2f3453f67796a267409a48985b2426cadb9179c4
unknown <Mukut.Prasad@eton-solutions.com>
2024-02-02 21:14:10 +0530

    Update CPVI to 1 hour

--- functions/src/common/models/CPVI.ts
+++ functions/src/common/models/CPVI.ts
@@ -566,7 +566,7 @@ export const CPVIForCoin = async (coinName: string) => {
     // get all the data in between24 hours
     const timeNow = Date.now();
     const currentTime = timeNow + 30;
-    const before24hoursTime = (currentTime - 24 * 7 * 3600000); //Update 24 hours to 7 days
+    const before24hoursTime = (currentTime - 1 * 1 * 3600000); //Update 24 hours to 7 days (Updated the again to 1 hour)
     console.log("current time: " + currentTime)
     console.log("before24hours time: " + before24hoursTime)
     const getAllCoinListing = (
