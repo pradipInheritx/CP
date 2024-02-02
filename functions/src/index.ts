1c38ad524c38db75abb52f589d4a003f4aca3c3f
Faizal Belim <faizal.b@inheritx.com>
2024-02-02 12:45:53 +0530

    comment function and add comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -64,7 +64,7 @@ import {
 } from "./common/models/PAX";
 import {
   claimReward,
-  addReward,
+  // addReward,
   cardHolderListing,
   // sendMintForPaxToAdmin,
   // sendMintForPaxToUser
@@ -786,6 +786,7 @@ exports.onUpdateUser = functions.firestore
     console.info("after", after)
     console.info("Send Email Successfully")
 
+    // for testing purposes, we comment this
     // await addReward(snapshot.after.id, before, after);
 
     const [should, amount] = shouldHaveTransaction(before, after);
