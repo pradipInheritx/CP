6fb0f1398f2fe238e301dcd9aaedbcfa86ecb150
unknown <Mukut.Prasad@eton-solutions.com>
2024-02-05 17:00:03 +0530

    Uncomment code for updateUserAfterPayment API

--- functions/src/routes/Payments.routes.ts
+++ functions/src/routes/Payments.routes.ts
@@ -7,7 +7,7 @@ import {
     getTransactionHistory,
     //makePaymentToServer,
     getParentPayment,
-    //updateUserAfterPayment,
+    updateUserAfterPayment,
     getInstantReferalAmount,
     callbackFromServer,
     //paymentStatusOnTransactionFromWellDApp,
@@ -22,7 +22,7 @@ const paymentRouter = Router();
 paymentRouter.post("/makePayment", makePayment);
 //paymentRouter.post("/makePayment/toServer", makePaymentToServer);
 paymentRouter.post("/makePayment/callback/fromServer", callbackFromServer);
-//paymentRouter.post("/update/user/afterVote", updateUserAfterPayment);
+paymentRouter.post("/update/user/afterPayment", updateUserAfterPayment);
 //paymentRouter.post("/update/paymentStatusOnTransaction/:transactionId", paymentStatusOnTransactionFromWellDApp);
 paymentRouter.post("/update/paymentStatusOnTransaction/fromUser/onCreditCard", paymentStatusOnUserFromCreditCard);
 paymentRouter.post("/make/createTempPaymentTransaction/onCreditCard", createPaymentOnTempTransactionOnCreditCard);
