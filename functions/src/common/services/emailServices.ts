a0301ae0756b0a812d0b0a9659341219b81e56cb
unknown <Mukut.Prasad@eton-solutions.com>
2024-02-06 15:51:45 +0530

    Change email address for user verify

--- functions/src/common/services/emailServices.ts
+++ functions/src/common/services/emailServices.ts
@@ -1,13 +1,13 @@
 import env from "../../env/env.json";
 import sgMail from "@sendgrid/mail";
 
-export async function sendEmail(to:any, subject:any, body:any) {
+export async function sendEmail(to: any, subject: any, body: any) {
   console.log("email>>>>>>>>");
 
   sgMail.setApiKey(env.sendgrid_api_key);
   const msg = {
     to,
-    from: "akashpatel.inheritx@gmail.com",
+    from: "admin@votetoearn.net",
     subject,
     html: body,
   };
