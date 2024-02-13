be83818b742b1d3a5d22b75ac2783171e0e74a80
Feni Patel <feni.p@inheritx.com>
2024-02-13 14:05:19 +0530

    added public links for the images

--- functions/src/common/emailTemplates/userWelcomeEmailTemplate.ts
+++ functions/src/common/emailTemplates/userWelcomeEmailTemplate.ts
@@ -56,7 +56,7 @@ export const userWelcomeEmailTemplate = (displayName: any, appLink: any) => {
                            <td align="center" valign="bottom" style="padding: 0px 0px 0px 0px;">
                               <a href="#" target="_blank"
                                  style="color:#000;text-decoration:none; display:inline-block;">
-                                 <img src="images/upbanner.jpg" alt="Coin Parliament Vote To Earn" class="inx_img"
+                                 <img src="https://firebasestorage.googleapis.com/v0/b/coin-parliament-staging.appspot.com/o/assests%2Femails%2FupBanner.jpg?alt=media&token=df5c2f20-3a41-4305-acd4-ab88e4709253" alt="Coin Parliament Vote To Earn" class="inx_img"
                                     width="600" height="234" border="0"
                                     style="display:block;font-family: 'Poppins', Arial, sans-serif;color:#ffffff;font-weight:bold;font-size:24px;max-width:600px;" />
                               </a>
@@ -236,7 +236,7 @@ export const userWelcomeEmailTemplate = (displayName: any, appLink: any) => {
                                              <td align="center" valign="bottom" style="padding: 0px 0px 0px 0px;">
                                                 <a href="https://t.me/VoteToEarn" target="_blank"
                                                    style="color:#000;text-decoration:none; display:inline-block;">
-                                                   <img src="images/telegram.png" alt="Coin Parliament Vote To Earn"
+                                                   <img src="https://firebasestorage.googleapis.com/v0/b/coin-parliament-staging.appspot.com/o/assests%2Femails%2Ftelegram.png?alt=media&token=dddf8117-7489-4291-b16d-e0a8d284e55d" alt="Coin Parliament Vote To Earn"
                                                       class="inx_telegram_btn" width="200" height="76" border="0"
                                                       style="display:block;font-family: 'Poppins', Arial, sans-serif;color:#ffffff;font-weight:bold;font-size:24px;max-width:200px;" />
                                                 </a>
@@ -293,7 +293,7 @@ export const userWelcomeEmailTemplate = (displayName: any, appLink: any) => {
                                              <td align="left" valign="middle" width="30" height="30">
                                                 <a href="https://linktr.ee/votetoearn" target="_blank"
                                                    style="text-decoration:none; display:inline-block;"><img
-                                                      src="images/instagram.svg" alt="instagram" width="30" height="30"
+                                                      src="https://firebasestorage.googleapis.com/v0/b/coin-parliament-staging.appspot.com/o/assests%2Femails%2Finstagram.png?alt=media&token=1acfe5c4-e570-45a1-b7e5-76ae7c4007af" alt="instagram" width="30" height="30"
                                                       border="0"
                                                       style="display:block;color: #ffffff;font-family: 'Poppins', Arial, sans-serif;font-size: 16px;line-height: 20px;"></a>
                                              </td>
@@ -301,7 +301,7 @@ export const userWelcomeEmailTemplate = (displayName: any, appLink: any) => {
                                              <td align="left" valign="middle" width="30" height="30">
                                                 <a href="https://linktr.ee/votetoearn" target="_blank"
                                                    style="text-decoration:none; display:inline-block;"><img
-                                                      src="images/facebook.svg" alt="facebook" width="30" height="30"
+                                                      src="https://firebasestorage.googleapis.com/v0/b/coin-parliament-staging.appspot.com/o/assests%2Femails%2Ffacebook.png?alt=media&token=c0669dc8-4542-476b-bc4e-b5bb4366fc1b" alt="facebook" width="30" height="30"
                                                       border="0"
                                                       style="display:block;color: #ffffff;font-family: 'Poppins', Arial, sans-serif;font-size: 16px;line-height: 20px;"></a>
                                              </td>
@@ -309,7 +309,7 @@ export const userWelcomeEmailTemplate = (displayName: any, appLink: any) => {
                                              <td align="left" valign="middle" width="30" height="30">
                                                 <a href="https://twitter.com/CoinParliament" target="_blank"
                                                    style="text-decoration:none; display:inline-block;"><img
-                                                      src="images/x-twitter.svg" alt="X Twitter" width="30" height="30"
+                                                      src="https://firebasestorage.googleapis.com/v0/b/coin-parliament-staging.appspot.com/o/assests%2Femails%2Fx-twitter.png?alt=media&token=5b0afbe6-f269-4ae0-9e2d-b05e01df6f85" alt="X Twitter" width="30" height="30"
                                                       border="0"
                                                       style="display:block;color: #ffffff;font-family: 'Poppins', Arial, sans-serif;font-size: 16px;line-height: 20px;"></a>
                                              </td>
@@ -318,7 +318,7 @@ export const userWelcomeEmailTemplate = (displayName: any, appLink: any) => {
                                              <td align="left" valign="middle" width="30" height="30">
                                                 <a href="https://linktr.ee/votetoearn" target="_blank"
                                                    style="text-decoration:none; display:inline-block;"><img
-                                                      src="images/tiktok.svg" alt="tiktok" width="30" height="30"
+                                                      src="https://firebasestorage.googleapis.com/v0/b/coin-parliament-staging.appspot.com/o/assests%2Femails%2Ftiktok.png?alt=media&token=5f4e633d-14dc-412f-9c2b-91480f1028e2" alt="tiktok" width="30" height="30"
                                                       border="0"
                                                       style="display:block;color: #ffffff;font-family: 'Poppins', Arial, sans-serif;font-size: 16px;line-height: 20px;"></a>
                                              </td>
