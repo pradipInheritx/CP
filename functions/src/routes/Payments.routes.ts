cda6a2ff6a14c1088e1d729a197827c9373603f0
Mukut Prasad <mukut@inheritx.com>
2024-02-20 12:23:10 +0530

    Remove unused WellDApp payment code

--- functions/src/routes/Payments.routes.ts
+++ functions/src/routes/Payments.routes.ts
@@ -2,15 +2,12 @@ import { Router } from "express";
 import { auth } from "../common/middleware/authentication";
 
 import {
-    makePayment,
     isUserUpgraded,
     getTransactionHistory,
-    //makePaymentToServer,
     getParentPayment,
     updateUserAfterPayment,
     getInstantReferalAmount,
     callbackFromServer,
-    //paymentStatusOnTransactionFromWellDApp,
     paymentStatusOnUserFromCreditCard,
     getAllPendingPaxByUser,
     createPaymentOnTempTransactionOnCreditCard
@@ -19,11 +16,8 @@ import { getPendingPaymentbyUserId } from "../common/models/Admin/payments"
 
 const paymentRouter = Router();
 
-paymentRouter.post("/makePayment", makePayment);
-//paymentRouter.post("/makePayment/toServer", makePaymentToServer);
 paymentRouter.post("/makePayment/callback/fromServer", callbackFromServer);
 paymentRouter.post("/update/user/afterPayment", updateUserAfterPayment);
-//paymentRouter.post("/update/paymentStatusOnTransaction/:transactionId", paymentStatusOnTransactionFromWellDApp);
 paymentRouter.post("/update/paymentStatusOnTransaction/fromUser/onCreditCard", paymentStatusOnUserFromCreditCard);
 paymentRouter.post("/make/createTempPaymentTransaction/onCreditCard", createPaymentOnTempTransactionOnCreditCard);
 paymentRouter.get("/isUserUpgraded/:userId", isUserUpgraded);
