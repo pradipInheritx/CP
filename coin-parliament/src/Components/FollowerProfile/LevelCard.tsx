335b5fa17726bde8ed80b89c9f8552de07b9e54e
Yauvan modi <yauvanmodi.uv@gmail.com>
2024-02-22 19:11:19 +0530

    followerProfile/mine card ui set

--- coin-parliament/src/Components/FollowerProfile/LevelCard.tsx
+++ coin-parliament/src/Components/FollowerProfile/LevelCard.tsx
@@ -92,7 +92,7 @@ const LevelCard = ({ userInfo, userTypes }: LevelCardProps) => {
         onMouseLeave={() => setTooltipShow(false)}
       ></I>
       <div className="d-flex justify-content-center align-items-center flex-column">
-        <h6 className="mt-1 box_title card-header mb-2" style={{ fontSize: '12px' }}>
+        <h6 className="mt-1 box_title card-header mb-2" style={{ fontSize: '12px', padding: "0.5rem 1rem"}}>
           {translate("User Level")}a
         </h6>
         {userTypes.sort((a, b) => b.index - a.index).map((userType, i) => {
