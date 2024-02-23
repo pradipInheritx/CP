197a9338b324f8998496e5dd4caeea3eea054a5e
Feni Patel <feni.p@inheritx.com>
2024-02-23 15:58:51 +0530

    added proper error handling into sendEmails function

--- functions/src/common/services/emailServices.ts
+++ functions/src/common/services/emailServices.ts
@@ -1,19 +1,29 @@
+
 import env from "../../env/env.json";
 import sgMail from "@sendgrid/mail";
 
 export async function sendEmail(to: any, subject: any, body: any) {
-  console.log("email>>>>>>>>");
+  try {
+    console.log("email>>>>>>>>");
 
-  sgMail.setApiKey(env.sendgrid_api_key);
-  const msg = {
-    to,
-    from: "support@votetoearn.net",
-    subject,
-    html: body,
-  };
-  const response = await sgMail.send(msg);
+    sgMail.setApiKey(env.sendgrid_api_key);
+    const msg = {
+      to,
+      from: "support@votetoearn.net",
+      subject,
+      html: body,
+    };
+    const response = await sgMail.send(msg);
 
-  console.log("RESPONSE EMAIL==>", response);
+    console.log("RESPONSE EMAIL==>", response);
 
-  console.log("Email sent");
+    console.log("Email sent");
+  } catch (error:unknown) {
+    if (error instanceof Error ){
+      console.error("Error sending email:", error.message);
+      throw new Error("Failed to send email");
+    }
+    
+  }
 }
+
