b92fcc4115defb4c222215f2b09e8b4c07187c32
Faizal Belim <faizal.b@inheritx.com>
2024-02-29 12:53:46 +0530

    get isVoteToEarn from user and change the variable name

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -385,7 +385,7 @@ exports.onCreateUser = functions.auth.user().onCreate(async (user: any) => {
     },
     favorites: [],
     status,
-    isVoteToEarn: user.isVoteToEarn || false,
+    // isVoteToEarn: user.isVoteToEarn || false,
     firstTimeLogin: true,
     refereeScrore: 0,
     lastVoteTime: 0,
@@ -400,21 +400,22 @@ exports.onCreateUser = functions.auth.user().onCreate(async (user: any) => {
       .firestore()
       .collection("users")
       .doc(user.uid)
-      .set(userData);
+      .set(userData,{merge: true});
 
-      const getUserEmail: any = (
+      const getUser: any = (
         await admin.firestore().collection("users").doc(user.uid).get()
       ).data();
-      console.log("new user email  : ", getUserEmail.email);
+      console.log("new user email  : ", getUser.email);
 
       //Send Welcome Mail To User
-      if (userData.isVoteToEarn === false) {
+      console.log("getUser.isVoteToEarn : ", getUser.isVoteToEarn)
+      if (getUser.isVoteToEarn === false) {
         await sendEmail(
           userData.email,
           "Welcome To Coin Parliament!",
           userWelcomeEmailTemplate(`${userData.userName ? userData.userName : 'user'}`, env.BASE_SITE_URL)
         );
-        await sendEmailVerificationLink(getUserEmail.email);
+        await sendEmailVerificationLink(getUser.email);
       }
     return newUser;
 
