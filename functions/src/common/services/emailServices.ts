f02c44f8a16a4443c2197ef5763e87d2d6efd4cb
bFaiz13 <faiz20b@gmail.com>
2024-04-01 13:21:34 +0530

    add username in email sender

--- functions/src/common/services/emailServices.ts
+++ functions/src/common/services/emailServices.ts
@@ -7,7 +7,10 @@ export async function sendEmail(to: any, subject: any, body: any) {
   sgMail.setApiKey(env.sendgrid_api_key);
   const msg = {
     to,
-    from: "support@sportparliament.com",
+    from: {
+      email: "support@coinparliament.com",
+      name: "Sport Parliament | V2E"
+  },
     subject,
     html: body,
   };
