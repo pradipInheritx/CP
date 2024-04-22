70dcb5671aab80602535f820af8e5527fe19779a
Manish Soni <manish.s@inheritx.com>
2024-04-22 08:28:11 +0000

    Update Login.ts

--- coin-parliament/src/common/models/Login.ts
+++ coin-parliament/src/common/models/Login.ts
@@ -101,10 +101,6 @@ export const LoginAuthProvider = async (
       // console.log('callback called for refeer',user)
       callback({ parent: refer, child: user.uid })
     }
-    await sendEmailVerificationLink({
-      email: user.email,
-    })
-
     if (isFirstLogin?.isNewUser) {
       // saveUsername(user.uid, '', '')
 
